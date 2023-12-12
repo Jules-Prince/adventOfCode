e12ba51759c75883af68171ea1752810166a067a
julesprince <jules.prince.fr@icloud.com>
2023-12-12 09:22:33 +0100

    day9

---
+++ 2023/day9/day9-part2.py
@@ -0,0 +1,31 @@
+
+sum = 0
+
+with open('/Users/julesprince/Library/Mobile Documents/com~apple~CloudDocs/Devs/adventOfCode/2023/day9/input/values.txt', 'r') as file:
+    lines = file.readlines()
+    
+    for line in lines :
+        allValues = []
+        values = line.split()
+        values = [eval(i) for i in values]
+        allValues.append(values)
+        nextValues = []
+        a = 0;
+        while len(nextValues) == 0 or not all(nextValue == 0 for nextValue in nextValues) :
+            nextValues = []
+            currentValues = allValues[a]
+            for i, value in enumerate(currentValues) :
+                if not (i+1>=len(currentValues))  :
+                    diff = currentValues[i+1] - value
+                    nextValues.append(diff)
+            allValues.append(nextValues)
+            a+=1
+        
+        allValues.pop()
+        computeValue = 0
+        for listValues in reversed(allValues):
+            computeValue = listValues[0] - computeValue
+            print("for list :", listValues, " compute value : ", computeValue)
+    
+        sum+=computeValue
+print("result : ", sum)
