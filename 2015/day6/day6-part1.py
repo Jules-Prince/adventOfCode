20f85761f4646116cbe0e8e8b45761629341a9c3
Jules Prince <jules.prince@se.com>
2023-12-14 16:21:17 +0100

    2015 day 6

---
+++ 2015/day6/day6-part1.py
@@ -0,0 +1,39 @@
+import os
+sum=0
+with (open(os.path.dirname(__file__) + '/input/instruction.txt', 'r')) as file:
+    lines = file.readlines()
+    lightGrid = [[0 for x in range(1000)] for y in range(1000)]
+    for line in lines:
+        
+        if "turn on" in line :
+            line = line.replace("turn on ", "")
+            line = line.replace("through ", "")
+            
+            start = tuple(map(int, line.split(" ")[0].split(",")))
+            end = tuple(map(int, line.split(" ")[1].split(",")))
+            
+            for x in range(start[0], end[0] + 1):
+                for y in range(start[1], end[1] + 1):
+                    lightGrid[x][y] = 1 
+        elif "turn off" in line :
+            line = line.replace("turn off ", "")
+            line = line.replace("through ", "")
+            start = tuple(map(int, line.split(" ")[0].split(",")))
+            end = tuple(map(int, line.split(" ")[1].split(",")))
+            for x in range(start[0], end[0] + 1):
+                for y in range(start[1], end[1] + 1):
+                    lightGrid[x][y] = 0
+        elif "toggle" in line : 
+            line = line.replace("toggle ", "")
+            line = line.replace("through ", "")
+            start = tuple(map(int, line.split(" ")[0].split(",")))
+            end = tuple(map(int, line.split(" ")[1].split(",")))
+            for x in range(start[0], end[0] + 1):
+                for y in range(start[1], end[1] + 1):
+                    lightGrid[x][y] = 0 if lightGrid[x][y] == 1 else 1
+
+for sublist in lightGrid:
+    for element in sublist:
+        if element == 1:
+            sum += 1
+print(sum)
